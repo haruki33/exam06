753d0a3c0d772aa32fa0cf0b20070c645a752c8d
haruki33 <kkhk0869hs33@gmail.com>
2026-05-19 16:46:36 +0900

    projectの初期実装

---
+++ src/main/java/com/example/Exam06Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exam06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exam06Application.class, args);
+	}
+
+}
